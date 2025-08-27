3e4f8bf769b7be0c44629ff4b1525e12065f0c95
eduardhu27 <eduardo.go5577@gmail.com>
2025-08-27 08:37:55 -0300

    correcao

---
+++ Q3.c
@@ -0,0 +1,66 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void multiplos( int n, int v,int multiAtual);
+
+void main() 
+{
+    int n,v;
+    
+        printf("Entre com um numero n:");
+        scanf("%d",&n);
+        printf("\nEntre com um limite V:");
+        scanf("%d",&v);
+    
+    multiplos(n,v,n);
+    
+}
+/*
+void multiplos( int n, int v)
+    {
+        int i=1,j=0;
+        while(j<v)
+        {
+            j=n*i;
+            printf("%d\n",j);
+            i++;
+        }
+         
+        
+    }
+    */
+void multiplos (int n, int v, int multiAtual)
+{
+    if(multiAtual<v)
+    {
+        printf("%d ",multiAtual);
+        multiplos(n,v,multiAtual+n);
+        
+    }
+    
+    
+}
+/*// Online C compiler to run C program online
+#include <stdio.h>
+
+void multiplos(int n, int v);
+void main()
+{
+    multiplos(3,20);
+}
+
+void multiplos(int n, int v)
+{
+    if(v>=n)
+    {
+        if(v%n==0)
+        {
+            multiplos(n,v-1);
+            printf("\t%d ",v);
+        }
+        else
+        {
+            multiplos(n,v-1);
+        }
+    }
+}*/
