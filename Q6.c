05ed3558f84ce3f2eec65cb59346b834a05eeea0
Eduardo G <eduardogomes5577@gmail.com>
2025-08-28 17:07:19 -0300

    Lista de recursividade

---
+++ Q6.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int palindromo (int i, int vet[], int quantidade);
+
+void main ()
+{
+    int quantidade,i,resposta ;
+    
+    printf("Entre com a quantidade de numeros no vetor:");
+    scanf("%d",&quantidade);
+    
+    int vet[quantidade];
+ for (i=0;i<quantidade;i++)
+    {
+        printf("Entre com um valor:");
+        scanf("%d",&vet[i]);
+    }
+    
+     resposta=palindromo(0,vet,quantidade);
+    if(resposta==1)
+    {
+    printf("Nao E um palindromo.");
+    }
+    else
+    {
+        printf("E um palindromo");
+    }
+}
+int palindromo (int i , int vet[], int quantidade)
+{
+    int tamanho=quantidade-1;
+
+        if(i<=tamanho)
+        {
+            if(vet[i]==vet[tamanho])
+            {
+           return palindromo(i+1,vet,quantidade-1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+                
+
+
+}
